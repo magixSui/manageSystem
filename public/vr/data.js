55973b40bd1c7459ee8539fea149e732a271bf64
MagixSui <suixin@jcinfo.online>
2020-03-06 15:26:08 +0800

    vr

---
+++ public/vr/data.js
@@ -0,0 +1,85 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-u15553912543645235621fm26gp0",
+      "name": "u=1555391254,3645235621&fm=26&gp=0",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 150,
+      "initialViewParameters": {
+        "yaw": -0.7920614643068351,
+        "pitch": 0.03269235828327055,
+        "fov": 1.3046490827601978
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.7920614643068351,
+          "pitch": 0.03269235828327055,
+          "rotation": 0,
+          "target": "1-timg-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.47051500445560634,
+          "pitch": 0.12178508017949952,
+          "title": "花瓶",
+          "text": "10块"
+        }
+      ]
+    },
+    {
+      "id": "1-timg-1",
+      "name": "timg (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 256,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.38710978722856204,
+          "pitch": 0.132110018913151,
+          "rotation": 0,
+          "target": "0-u15553912543645235621fm26gp0"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3681678984637884,
+          "pitch": 0.021676298053279197,
+          "title": "电视",
+          "text": "1000R"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
